3144b67a0a37dcb387b97f07b71dee68928e15ec
AishwaryaLPatil <aishwaryalp166@gmail.com>
2024-11-10 14:22:22 +0530

    loading

--- train_prompt1_seed12.sh
+++ train_prompt1_seed12.sh
@@ -2,4 +2,4 @@
 model_name='ProTACT'
 prompt=1
 seed=12
-python train_ProTACT.py --test_prompt_id ${prompt} --model_name ${model_name} --seed ${seed} --num_heads 2 --features_path 'data/LDA/hand_crafted_final_'
+python train_Protact_new.py --test_prompt_id ${prompt} --model_name ${model_name} --seed ${seed} --num_heads 2 --features_path 'data/LDA/hand_crafted_final_'
